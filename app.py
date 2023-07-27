7aa279b3aeab02e393c8ce1d354729d5c3178f75
Arjita Singh Kushwaha <94288939+Arjitakushwah@users.noreply.github.com>
2023-07-27 22:20:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+#help sqlalchamy to connect with database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sample.sqlite3"
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Brand(db.Model):
+    b_id = db.Column(db.Integer, primary_key = True)
+    b_name = db.Column(db.String(30),nullable = False)
+
+class Car(db.Model):
+    c_id = db.Column(db.Integer, primary_key = True)
+    c_name = db.Column(db.String(30),nullable = False)
+
+
+#CRUD
+b1 = Brand(b_name = "TESLA" )#create
+c1 = Car(c_name = "model S")
+db.session.add_all([b1,c1])
+db.session.commit()
+mycar = Car.query.all()#read
+second = mycar[0]
+second.c_name = "model z" #update
+db.session.commit()
+to_delete = Car.query.filter_by(c_name = "model z").first()
+db.session.delete(to_delete)#delete
+db.session.commit()
